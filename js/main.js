bae34fda4bb6a1bd0f7ed36d2913f99351a0b73e
lukaszcwyl <lukasz.cwyl@commart.pl>
2021-07-27 22:35:11 +0200

    Wypisanie w consoli czy mieści liczba mieści się w przedziale 100 do 200

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const wskaz = (n) =>{
+    if(n >= 100 && n <= 200){
+        console.log("liczba znajduję się w przedziale");
+    }
+    else{
+        console.log("liczba nie znajduję się w przedziale"); 
+    }
+}
+let wynik = wskaz(100);
